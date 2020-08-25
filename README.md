c03e08a82998275fad416a623f570edb5e91ade3
Hebo <hebo@hey.com>
2020-08-25 14:25:16 -0700

    cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spotcast
+
+Download podcasts from Spotify
+
+## Usage
+
+1. Set `.env` to your Spotify OAuth token (see `.env.example`) and edit `SHOW_URL` in `spotcast.py`
+
+2. Run the script
+
+```
+./spotcast.py
+```
--- spotcast.py
+++ spotcast.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 
 import os
-import requests
 from urllib.parse import urlparse
 from pathlib import Path
 
+import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-
-
 load_dotenv()
 
 SHOW_URL = "https://open.spotify.com/show/2G3qmhWe8reqyCc7SRqI8F"
 DOWNLOAD_PATH = "./downloads"
-
-print(SHOW_URL, os.getenv("SPOTIFY_TOKEN"))
-
-# print(response.text)
-
 
 def get_episodes(url):
     headers = {
@@ -30,43 +23,45 @@ def get_episodes(url):
 
     meta = soup.findAll("meta", {"property": "music:song"})
     url_list = [tag["content"] for tag in meta]
-    return [urlparse(url).path.split('/')[-1] for url in url_list]
+    return [urlparse(url).path.split("/")[-1] for url in url_list]
+
+
+def sanitize_filename(name):
+    return "".join(x for x in name if (x.isalnum() or x in "._- "))
 
 
 def fetch_episode(episode_id):
-    print(f"Fetching episode {episode_id}")
 
     url = "https://api.spotify.com/v1/episodes/" + episode_id
     headers = {
-        'Authorization': "Bearer {}".format(os.getenv("SPOTIFY_TOKEN")),
-        'Origin': 'https://open.spotify.com',
-        'Referer': 'https://open.spotify.com/show/2G3qmhWe8reqyCc7SRqI8F',
-        'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
+        "Authorization": "Bearer {}".format(os.getenv("SPOTIFY_TOKEN")),
+        "Origin": "https://open.spotify.com",
+        "Referer": "https://open.spotify.com/show/2G3qmhWe8reqyCc7SRqI8F",
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
     }
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     parsed = response.json()
 
+    print(f"Fetching episode {episode_id} - {parsed['name']}")
     download_url = parsed["external_playback_url"]
-    filename = "{}.mp3".format(parsed["name"])
+    filename = "{}.mp3".format(sanitize_filename(parsed["name"]))
 
-
-
-    print(download_url)
+    print(" --> " + download_url)
     headers = {
-        'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15',
-        'Referer': 'https://open.spotify.com/show/2G3qmhWe8reqyCc7SRqI8F',
-        'Accept-Encoding': 'identity',
-        'Connection': 'Keep-Alive',
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+        "Referer": "https://open.spotify.com/show/2G3qmhWe8reqyCc7SRqI8F",
+        "Accept-Encoding": "identity",
+        "Connection": "Keep-Alive",
     }
 
     episode = requests.get(download_url, headers=headers)
     file_dir = Path(DOWNLOAD_PATH)
     file_dir.mkdir(parents=True, exist_ok=True)
-    with open( file_dir / filename, 'wb') as f:
+    with open(file_dir / filename, "wb") as f:
         f.write(episode.content)
 
 
@@ -74,5 +69,5 @@ episodes = get_episodes(SHOW_URL)
 print(f"Found {len(episodes)} episodes")
 print(episodes)
 
-
-fetch_episode(episodes[1])
+for ep in episodes:
+    fetch_episode(ep)
